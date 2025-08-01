9d3988197091c8da47d0efd1480a027e3c1766cb
Pradnyesh Gudadhe <pgudadhe@users.noreply.github.com>
2025-07-31 20:52:12 -0700

    Delete build directory

--- build/CMakeFiles/AXI_TLM_PERF_MODEL.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for AXI_TLM_PERF_MODEL.
--- build/CMakeFiles/AXI_TLM_PERF_MODEL.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for AXI_TLM_PERF_MODEL.
-# This may be replaced when dependencies are built.
